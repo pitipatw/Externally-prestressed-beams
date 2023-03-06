e10c55c09cff2991550c8910a4ba8a53a5025214
pitipat_mac <pitipatw@mit.edu>
2023-03-06 18:33:32 -0500

    update Parameters and debugging

--- LinearUncrack.jl
+++ LinearUncrack.jl
@@ -8,26 +8,34 @@ Adopted from
 
 #Inputs
 #Units N, mm, MPa
-begin
-fc′ = 35. #MPa 
+#   Material Properties
+    fc′= 30. # Concrete strength [MPa] 
+    Ec = 4700.0*sqrt(fc′) # MPa  ACI fc-> Concrete modulus relationship [MPa]
+    Eps = 70000.0 #Post tensioning steel modulus [MPa]
+    fpy = 0.002*Eps #MPa  
+    #Safe load on the website https://www.engineeringtoolbox.com/wire-rope-strength-d_1518.html 
+    # is ~ 150 MPa.
+# PixelFrame section/element properties
 
-Ec = 4700.0*sqrt(fc′) #MPa  ACI fc-> E relationship
-Eps = 70000.0 #MPa
-fpy = 0.002*Eps
-em = 228.9 #mm
-es = 0.
-e = em # this is special
-Ls = 502.7 #mm
-Ld = Ls
-# two 1/4" bars with 1200 lb capacity
-Aps = 2.0*0.25*25.4^2*pi/4.0 #mm2
-fpe = 0.1*fpy# will input the one on the test day.
-Zb = 452894.24 #mm3 elastic modulus
-Atr = 18537.69 # mm2 Transformed area of crosssection.
-Itr = 6.4198e+07 #mm4 moment of inertia 
-w = Atr/10^9*2400.0*9.81 # N/mm
-Mg = w*2000.0^2/8.0 # Nmm
-L = 2000.0 #mm
+    em = 228.9 #mm
+    es = 0.
+    em0 = em # Initial eccentricity at the midspan
+    Ls = 502.7 #mm
+    Ld = Ls
+    L = 2000.0 #mm
+    # two 1/4" bars with 1200 lb capacity
+    Aps = 2.0*(0.25*25.4)^2*pi/4.0 #mm2
+    fpe = 0.1*fpy # MPa will input the one on the test day.
+    Zb = 452894.24 #mm3 elastic modulus
+    Atr = 18537.69 # mm2 Transformed area of crosssection.
+    Itr = 6.4198e+07 #mm4 moment of inertia 
+
+    #forces
+    w = Atr/10^9*2400.0*9.81 # N/mm
+    Mg = w*2000.0^2/8.0 # Nmm
+    fr = 0.7*sqrt(fc′) #MPa
+    r  = sqrt(Itr/Atr) #mm
+
 #for Ld == Ls (our test) 
 #eccentricity measured from the neutral axis
 # es is eccentricity at the support
@@ -46,18 +54,17 @@ L = 2000.0 #mm
 # which type is faster, Real or Float64?
 #
 
-fr = 0.7*sqrt(fc′) #MPa
-r  = sqrt(Itr/Atr)
+
 function getOmega(Ls::Float64, L::Float64, es::Float64, em::Float64)
-    omega = 1-(es/em)+(es-em)/em*(Ls/L*4/3)
+    omega = 1.0 - (es / em)*(Ls/L) + (es - em)/ em * (Ls/L*4/3)
     return omega
 end
 
 #calculate the moment at the critical section
 function getMcr(em::Float64, Mg::Float64, omega::Float64, Itr::Float64, Ec::Float64, Eps::Float64, Aps::Float64, r::Float64, Zb::Float64, Atr::Float64)
-    mcre = Aps*fpe*(em + Zb/Atr) + fr*Zb
+    @show mcre = Aps*fpe*(em + Zb/Atr) + (fr * Zb)
     @assert mcre > 0
-    dmcr = (Aps*em*(em + Zb / Atr)*(mcre - Mg)) / ((1/omega*Itr*Ec/Eps) + Aps*(r^-em*Zb/Atr))
+    @show dmcr = (Aps*em*(em + Zb / Atr)*(mcre - Mg)) / ((1/omega*Itr*Ec/Eps) + Aps*(r^-em*Zb/Atr))
     @assert dmcr > 0
     mcr = mcre + dmcr
     return mcr
@@ -66,30 +73,35 @@ end
 #pass these 2 lines first
 omega = getOmega(Ls, L, es, em)
 mcr = getMcr(em, Mg, omega, Itr, Ec, Eps, Aps, r, Zb, Atr) # Nmm
-end
 ##
 # Under Mcr fps will follow this equation 
 # M here is the Moment at the critical section (variable)
 # so this is a function between fps and M,e (e is the middle of the beam)
 
-function getDelta(M::Float64, e::Float64, L::Float64, Ls::Float64, es::Float64, em::Float64, Itr::Float64, Ec::Float64, Eps::Float64, Aps::Float64, r::Float64, Zb::Float64, Atr::Float64)
+function getDelta(fps::Float64, M::Float64, L::Float64, Ls::Float64, es::Float64, em::Float64, Itr::Float64, Ec::Float64, Aps::Float64)
     #displacement 
     # due to the PS force
     #at mid span
-    delta_mid_neg = fps*Aps/(Ec*Itr)*(e*L^2/8 - (e-es)*Ls^2/6)
-
+    delta_mid_neg = fps*Aps/(Ec*Itr) * (em * L^2 / 8 - (em-es)*Ls^2/6)
     # at deviator 
-    delta_dev_neg = fps*Aps/(Ec*Itr)*(es*Ls^2/6 + e*(L*Ls/2-2/3*Ls^2))
+    delta_dev_neg = fps*Aps/(Ec*Itr)*(es*Ls^2/6 + em*(L*Ls/2-2/3*Ls^2))
 
     # due to the applied force
+    #at the mid span
     delta_mid_pos = M*L^2/(6*Ec*Itr)*(3/4-(Ls/L)^2)
+    # at a deviator
     delta_dev_pos = M*L^2/(6*Ec*Itr)*( 3*(Ls/L)*(1-Ls/L)-(Ls/L)^2)
 
     delta_mid = delta_mid_pos - delta_mid_neg
-    Δ = delta_mid - delta_dev_pos + delta_dev_neg
-    Δcalc = M*L^2/(6*Ec*Itr)*(3/4) - fps*Aps*e/(Ec*Itr)*(L^2/8 - L*Ls/2 + Ls^2/2)
-    e = em - ( delta_mid - delta_dev_pos + delta_dev_neg )
-   # @assert Δ == Δcalc
+    delta_mid_calc = M*L^2/(6*Ec*Itr)*(3/4-(Ls/L)^2) - fps*Aps/(Ec*Itr) * (em * L^2 / 8 - (em-es)*Ls^2/6)
+    @assert abs(delta_mid - delta_mid_calc) < 1e-3
+    Δ = delta_mid - (delta_dev_pos - delta_dev_neg)
+    K1 = Ls/L-1 
+    K2 = 0.0
+    Δcalc = M*L^2/(6*Ec*Itr)*(3 * (Ls/L) * K1 + 3/4 + K2) - fps*Aps*em/(Ec*Itr)*(L^2/8 - L*Ls/2 + Ls^2/2)
+
+    # @show Δ - Δcalc
+    @assert abs(Δ - Δcalc) < 1e-6
     return Δ
 end
 
@@ -99,52 +111,63 @@ end
 
 #Evaluate the deflection at the midspan
 #Given a value of M(t) as an input (increments) -> get this from load graph. 
-P = 0.:0.1:100.  # Dummy load as from 0 to 100 with 0.1 increment
-Ls = 50. # distance from support to the loading point.
 
+# Dummy load as from 0 to 6600 N (1.5kips) with 0.1 increment
+P = 0.:10:10600.  # N
 M = P*Ls/2.
 displacements = zeros(length(M))
+displacements_mid_pos = zeros(length(M))
+iteration_exceeded = zeros(length(M)) 
 fps = fpe # initial guess of the stress in the tendons.
 omega = getOmega(Ls, L, es, em)
 fps_old = fpe #will have to update at the end as the current fps and use that in the next loop.
+max_it = 1000
+fps_sub_hist = zeros(max_it)
+fps_history = zeros(length(M))
 #loop M
 for i in eachindex(M)
     Mi = M[i] #value of the current applied moment
     if Mi <= mcr
-        print(mcr)
+        #Better format here
+        #print mcr with long format
+        println("Moment is less than the critical moment")
+        println("M = $Mi, Mcr = $mcr")
 
-    #M0 = 0.1 
-    #Assume fps = fpe as an initial guess
     
+        #Assume fps = fpe as an initial guess
         check_term = 1.
         tol = 1e-6
-        max_it = 1000
+   
         it = 0
         while check_term > tol
             it += 1 
             if it > max_it
                 println("Maximum number of iterations reached for M = $Mi")
+                iteration_exceeded[i] = 1
                 break
             end
             #calculate the total deflection
-            Δ = getDelta(Mi, e, L, Ls, es, em, Itr, Ec, Eps, Aps, r, Zb, Atr)
+            Δ = getDelta(fps_old,Mi, L, Ls, es, em, Itr, Ec, Aps)
 
             #find the actual eccentricity of the tendon
             e = em - Δ 
 
             #calculate the stress in the tendons, limited by fpy
-            fps_new = clamp(fpe +(omega*Mi*e)/(Itr*Ec/Eps+ Aps*(r^2+e^2)*omega) , fpe , fpy)
-
-            check_term = abs(fps_new - fps)/fps
-
+            fps_new = fpe +(omega*Mi*e)/(Itr*Ec/Eps + Aps*(r^2+e^2)*omega)
+            if fps_new > fpy 
+                println("Exceeds the yielding stress")
+                println("Beam reaching Ultimate Stress capacity (fpy) and will fail")
+                break
+            end
+            check_term = abs(fps_new - fps_old)/fps_old
+            fps_sub_hist[it] = fps_new
             fps_old = fps_new # this one used for the next loop.
         end
 
-        @show delta_mid_neg = fps*Aps/(Ec*Itr)*(e*L^2/8 - (e-es)*Ls^2/6)
-        @show delta_mid_pos = Mi*L^2/(6*Ec*Itr)*(3/4-(Ls/L)^2)
-        @show delta_mid = delta_mid_pos - delta_mid_neg
-        @show displacements[i] = delta_mid
-    elseif Mi > Mcr && Mi <= My
+        displacements[i] = getDelta(fps_old,Mi, L, Ls, es, em, Itr, Ec, Aps)
+        displacements_mid_pos[i] = Mi*L^2/(6*Ec*Itr)*(3/4-(Ls/L)^2)
+        fps_history[i] = fps_old
+    elseif Mi > mcr 
         println("Exceeds the cracking moment")
         println("Using Linear Crack scheme")
     elseif Mi > My
@@ -153,11 +176,11 @@ for i in eachindex(M)
     else
         println("Something is wrong")
     end
-    
 end
+fps_sub_hist
+#plot the deflection
+using Plots
 
-#repeat the process
+plot(P, displacements_mid_pos, label = "Deflection")
 
-#After it converges,
-#Calculate the deflection
-
+plot(P, fps_history, label = "Stress")
